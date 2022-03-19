fa350e80903e817d3d5c6c0ab330dda595864056
Karolina Mizgier <karolinamizgier@gmail.com>
2022-03-19 22:38:27 +0100

    Zadanie: dodanie maila do wiadomości (ang. CC)

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,13 +1,16 @@
 package com.crud.tasks;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TasksApplicationTests {
 
+	@Value( "${spring.mail.username}" )
+	private String username;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
